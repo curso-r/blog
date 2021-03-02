bb39af38708804afb59d06be1ec7442a5ff78518
williamorim <thewilliam89@gmail.com>
2021-03-02 20:17:11 -0300

    post brasileirao

--- utils.R
+++ utils.R
@@ -6,3 +6,12 @@ imagem_post_feature <- function(file) {
   )
   magick::image_write(img, file)
 }
+
+imagem_post <- function(file) {
+  img <- magick::image_crop(
+    magick::image_read(file),
+    geometry = "724x386!",
+    gravity = "Center"
+  )
+  magick::image_write(img, file)
+}
